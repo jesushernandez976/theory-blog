1da42439ed581b3c1091a0538afe9caa541b48d1
jesushernandez976 <jp9000xx@outlook.com>
2025-08-27 16:22:08 -0500

    updates 2

--- backend/index.js
+++ backend/index.js
@@ -12,17 +12,19 @@ import bodyParser from "body-parser";
 
 const app = express();
 
+
 import dotenv from 'dotenv';
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 app.use("/webhooks", webhookRouter);
-app.use(express.json());
 
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.json());
 app.use(clerkMiddleware());
 app.use(cors(process.env.CLIENT_URL));
+
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
